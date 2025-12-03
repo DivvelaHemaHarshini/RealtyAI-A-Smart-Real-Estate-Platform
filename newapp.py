20ce497e129808adceb2eee2f92a9291be9b655b
Divvela Hema Harshini <harshinidivvela0@gmail.com>
2025-12-03 20:22:08 +0530

    newapp.py

--- newapp.py
+++ newapp.py
@@ -1319,5 +1319,6 @@ if st.session_state.logged_in:
 else:
     login_signup_page()
 
-st.write("RealtyAI-A Smart Real estate Platform"|Powered By HarshiniDivvela 2025")
+st.write("RealtyAI-A Smart Real estate Platform"|Powered By HarshiniDivvela")
+
 
